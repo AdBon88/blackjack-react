65e0eea30f7391e037ebeff86a0cdc97e6877e79
Adam B <adam.bonarrigo@myob.com>
2021-01-11 18:28:29 +1100

    Trying to get a bloody button working

---
+++ ClientApp/src/components/AddItemModal.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import './modal.css';
+
+
+const AddItemModal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <button type="button" onClick={handleClose}>
+          Close
+        </button>
+      </section>
+    </div>
+  );
+};
+export default AddItemModal;
